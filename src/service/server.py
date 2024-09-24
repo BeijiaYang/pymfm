f9a6e9896e8df73553b67fec0be54cedb5b41f48
Florian Oppermann <florian.oppermann@eonerc.rwth-aachen.de>
2024-09-24 11:38:09 +0000

    chore: remove unused router
    Signed-off-by: Florian Oppermann <florian.oppermann@eonerc.rwth-aachen.de>

--- src/service/server.py
+++ src/service/server.py
@@ -51,55 +51,10 @@ def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
 
 app = FastAPI()
 
-balancing_router_old = APIRouter(
-    tags=["balancing"],
-)
 
 # XXX handle this via settings in the future
 storage = FileStorage(filepath=Path(__file__).parent / "store")  # MemoryStorage() # RedisStorage()
 # storage = RedisStorage()
-
-
-@balancing_router_old.post("/")
-async def create_balancing_task(input: data_input.InputData, background_tasks: BackgroundTasks) -> JobComplete:
-    log.info(f"received input data with id=<{input.id}>. Starting algorithm...")
-    job = JobComplete(id=input.id, input=input)
-    await storage.store(job)
-    background_tasks.add_task(scheduling_or_real_time, job=job, storage=storage)
-    return job
-
-
-@balancing_router_old.get("/", description="get the ids of all jobs")
-async def get_ids_endpoint() -> List[str]:
-    return await storage.all_ids()
-
-
-@balancing_router_old.get("/{id}")
-async def get_result_endpoint(id: str) -> JobComplete:
-    output = await storage.read(id)
-    if output is None:
-        raise HTTPException(404, f"No job with id {id}")
-    return output
-
-
-# XXX is Updating of a task usefull? Does the job need to be finished? Does a data update replace the data or append it? etc
-# @balancing_router.put("/{id}")
-# async def put_input_endpoint(input: data_input.InputData):
-#     start = timer()
-#     result_valid = False
-#     result = balancer_control(input)
-#     output = data_output.df_to_output(result, input.id)
-#     log.info(f"received input data with id=<{input.id}>. Starting algorithm...")
-#     return {"success": True}
-
-
-@balancing_router_old.delete("/{id}")
-async def delete_result_endpoint(id: str) -> JobComplete:
-    try:
-        result = await storage.delete(id)
-    except HTTPException as exc:
-        raise exc
-    return result
 
 
 @app.get("/health")
